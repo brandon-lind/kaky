f0da5e1a99c73055aa7258cdafa4c3fb1a70a29c
Brandon Lind <brandon@lindsoftware.com>
2020-07-12 10:04:15 -0500

    Assignment link disable capability

--- src/assets/scripts/components/workitems.js
+++ src/assets/scripts/components/workitems.js
@@ -17,13 +17,51 @@ class WorkItems {
       </div>
     </div>`;
     this.workItemPriceEditorTemplate = `
+    <h5 class="card-title">Set a price for <i></i></h5>
+    <p class="card-text">Use the defult price or negotiate a new one.</p>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
         <span class="input-group-text">$</span>
       </div>
-      <input type="text" class="form-control" aria-label="Negotiated Amount (to the nearest dollar)" />
+      <input type="text" class="form-control" aria-label="Negotiated Amount" />
     </div>
     `;
+  }
+
+  createWorkItemNode(item, template, allowAssignment = true) {
+    const listItem = template.content.cloneNode(true);
+
+    const img = listItem.querySelector('img');
+    img.src = item.imageUrl;
+
+    if (allowAssignment) {
+      img.onclick = () => { window.location = `${this.assignWorkUrl}?id=${item.id}`;};
+      img.style.cursor = 'pointer';
+    }
+
+    const title = listItem.querySelector('h1');
+    title.innerHTML = item.name;
+
+    const price = listItem.querySelector('h2');
+    price.innerHTML = `&#36;${item.price}`;
+
+    const tasks = listItem.querySelector('ul');
+
+    item.tasks.forEach(task => {
+      const li = document.createElement('li');
+      li.innerHTML = task;
+      tasks.appendChild(li);
+    });
+
+    const assignButton = listItem.querySelector('a.btn');
+
+    if (allowAssignment) {
+      assignButton.href = `${this.assignWorkUrl}?id=${item.id}`;
+    } else {
+      assignButton.remove();
+    }
+
+    return listItem;
   }
 
   async fetchWorkItems(overrideCache = false) {
@@ -48,9 +86,28 @@ class WorkItems {
   async renderList(targetEl) {
     if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to add work items into.');
 
+    const template = document.createElement('template');
+    template.innerHTML = this.workItemTemplate;
+    let counter = 0;
+    let cardDeck;
     let workItems = await this.fetchWorkItems();
 
-    this.renderWorkItems(targetEl, workItems);
+    workItems.forEach(item => {
+      if (counter === 0) {
+        cardDeck = document.createElement('div');
+        cardDeck.classList.add('card-deck', 'mb-3', 'text-center');
+        targetEl.appendChild(cardDeck);
+      }
+
+      if (counter === (this.itemLimit - 1)) {
+        counter = 0;
+      } else {
+        counter++;
+      }
+
+      let card = this.createWorkItemNode(item, template);
+      cardDeck.appendChild(card);
+    });
   }
 
   async renderPriceEditor(targetEl, id) {
@@ -66,66 +123,26 @@ class WorkItems {
     const editor = template.content.cloneNode(true);
 
     const inputBox = editor.querySelector('input');
-    inputBox.value = `${workItem.price}`;
+    inputBox.value = `${isNaN(workItem.price) ? 1 : workItem.price}`;
+
+    const itemDesc = editor.querySelector('i');
+    itemDesc.innerHTML = `${workItem.name}`;
 
     targetEl.appendChild(editor);
   }
 
-  async renderWorkItem(targetEl, id) {
+  async renderWorkItem(targetEl, id, allowAssignment = true) {
     if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to the add work item to.');
+
+    const template = document.createElement('template');
+    template.innerHTML = this.workItemTemplate;
 
     let workItem = await this.findWorkItem(id);
 
     if (!workItem) throw new Error('The work item does not exist.');
 
-    this.renderWorkItems(targetEl, new Array(workItem));
-  }
+    let workItemNode = this.createWorkItemNode(workItem, template, allowAssignment);
 
-  renderWorkItems(targetEl, items) {
-    if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to add work items into.');
-
-    const template = document.createElement('template');
-    template.innerHTML = this.workItemTemplate;
-    let counter = 0;
-    let cardDeck;
-
-    items.forEach(item => {
-      const card = template.content.cloneNode(true);
-
-      if (counter === 0) {
-        cardDeck = document.createElement('div');
-        cardDeck.classList.add('card-deck', 'mb-3', 'text-center');
-        targetEl.appendChild(cardDeck);
-      }
-
-      if (counter === (this.itemLimit - 1)) {
-        counter = 0;
-      } else {
-        counter++;
-      }
-
-      const a = card.querySelector('a');
-      a.href = `${this.assignWorkUrl}?id=${item.id}`;
-
-      const img = card.querySelector('img');
-      img.src = item.imageUrl;
-      img.onclick = () => { window.location = `${this.assignWorkUrl}?id=${item.id}`;};
-
-      const title = card.querySelector('h1');
-      title.innerHTML = item.name;
-
-      const price = card.querySelector('h2');
-      price.innerHTML = `&#36;${item.price}`;
-
-      const tasks = card.querySelector('ul');
-
-      item.tasks.forEach(task => {
-        const li = document.createElement('li');
-        li.innerHTML = task;
-        tasks.appendChild(li);
-      });
-
-      cardDeck.appendChild(card);
-    });
+    targetEl.appendChild(workItemNode);
   }
 }
