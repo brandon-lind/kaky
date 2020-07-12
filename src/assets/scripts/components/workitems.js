8cd3f6e9d10cf306b8d155b63a461387ef63d9f9
Brandon Lind <brandon@lindsoftware.com>
2020-07-12 08:22:50 -0500

    Adding a work item price component

--- src/assets/scripts/components/workitems.js
+++ src/assets/scripts/components/workitems.js
@@ -1,9 +1,10 @@
 class WorkItems {
   constructor(itemLimitPerRow) {
     this.itemLimit = itemLimitPerRow || 3;
+    this.workItems = [];
     this.workItemsUrl = './assets/data/workitems.json';
     this.assignWorkUrl = './assign.html'
-    this.template = `
+    this.workItemTemplate = `
     <div class="card mb-4 shadow-sm">
       <img class="img-fluid card-img-top" />
       <div class="card-body d-flex flex-column">
@@ -15,23 +16,75 @@ class WorkItems {
         <a class="btn btn-lg btn-block btn-primary mt-auto">Assign</a>
       </div>
     </div>`;
+    this.workItemPriceEditorTemplate = `
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">$</span>
+      </div>
+      <input type="text" class="form-control" aria-label="Negotiated Amount (to the nearest dollar)" />
+    </div>
+    `;
   }
 
-  render(targetEl) {
-    if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to add work items.');
+  async fetchWorkItems(overrideCache = false) {
+    if (this.workItems && this.workItems.length && !overrideCache) return;
 
-    fetch(this.workItemsUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.createWorkItems(targetEl, data);
-      });
+    let response = await fetch(this.workItemsUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error fetching work items! status: ${response.status}`);
+    } else {
+      this.workItems = await response.json();
+    }
   }
 
-  createWorkItems(targetEl, items) {
+  async findWorkItem(id) {
+    await this.fetchWorkItems();
+
+    return this.workItems.find(x => x.id === id);
+  }
+
+  async renderAll(targetEl) {
+    if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to add work items into.');
+
+    await this.fetchWorkItems();
+
+    this.renderWorkItems(targetEl, data);
+  }
+
+  async renderPriceEditor(targetEl, id) {
+    if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to the add work item price editor into.');
+
+    let workItem = await this.findWorkItem(id);
+
+    if (!workItem) throw new Error('The work item does not exist. Cannot render price editor.');
+
     const template = document.createElement('template');
-    template.innerHTML = this.template;
+    template.innerHTML = this.workItemPriceEditorTemplate;
+
+    const editor = template.content.cloneNode(true);
+
+    const inputBox = editor.querySelector('input');
+    inputBox.value = `${workItem.price}`;
+
+    targetEl.appendChild(editor);
+  }
+
+  async renderWorkItem(targetEl, id) {
+    if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to the add work item to.');
+
+    let workItem = await this.findWorkItem(id);
+
+    if (!workItem) throw new Error('The work item does not exist.');
+
+    this.renderWorkItems(targetEl, new Array(workItem));
+  }
+
+  renderWorkItems(targetEl, items) {
+    if (!targetEl || !targetEl.innerHTML) throw new Error('There is no target element to add work items into.');
+
+    const template = document.createElement('template');
+    template.innerHTML = this.workItemTemplate;
     let counter = 0;
     let cardDeck;
 
@@ -55,6 +108,7 @@ class WorkItems {
 
       const img = card.querySelector('img');
       img.src = item.imageUrl;
+      img.onclick = () => { window.location = `${this.assignWorkUrl}?id=${item.id}`;};
 
       const title = card.querySelector('h1');
       title.innerHTML = item.name;
