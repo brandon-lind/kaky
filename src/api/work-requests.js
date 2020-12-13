3a69cec098fb032d158b9dac06998a4383184d58
Brandon Lind <brandon@lindsoftware.com>
2020-12-13 14:20:54 -0600

    Finalizing fixes

--- src/api/work-requests.js
+++ src/api/work-requests.js
@@ -122,9 +122,9 @@ app.post(`${basePath}/`, validateUser, async (req, res) => {
     console.error(e);
 
     if (e.message.indexOf('WorkRequest validation failed:') !== -1) {
-      res.status(400).json({ message: e.message, data: e.message });
+      res.status(400).json({ message: e.message, data: null });
     } else {
-      res.status(500).json({ message: `Hm, that broke something when submitting.`, data: {m: e.message, u: process.env.MONGODB_URI} });
+      res.status(500).json({ message: `Hm, that broke something when submitting.`, data: null });
     }
   }
 });
