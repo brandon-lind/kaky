3460e9acd35c5082dbf6292096348383cebc7eeb
Brandon Lind <brandon@lindsoftware.com>
2020-12-13 13:30:40 -0600

    Fixing role check

--- src/api/work-requests.js
+++ src/api/work-requests.js
@@ -122,9 +122,9 @@ app.post(`${basePath}/`, validateUser, async (req, res) => {
     console.error(e);
 
     if (e.message.indexOf('WorkRequest validation failed:') !== -1) {
-      res.status(400).json({ message: e.message, data: null });
+      res.status(400).json({ message: e.message, data: e });
     } else {
-      res.status(500).json({ message: `Hm, that broke something.`, data: null });
+      res.status(500).json({ message: `Hm, that broke something.`, data: e });
     }
   }
 });
