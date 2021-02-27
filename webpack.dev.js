8152aa7f44f6cfa86755ae77727e94b6adb8d1d1
EC2 Default User <ec2-user@ip-172-31-11-10.us-east-2.compute.internal>
2021-02-27 23:31:39 +0000

    Changes to support Cloud9 IDE

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,9 +5,17 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    allowedHosts: [
+      '.amazonaws.com'
+    ],
     contentBase: './dist-ui',
+    host: '0.0.0.0',
     writeToDisk: true,
     proxy: {
+      '/.netlify/identity': {
+        target: 'https://staging.kaky.us',
+        changeOrigin: true
+      },
       '/.netlify/functions': {
         target: 'http://localhost:9000'
       }
