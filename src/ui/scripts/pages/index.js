435fc8fc9090926461a33f04752d665d3174765b
Brandon Lind <brandon@lindsoftware.com>
2021-04-17 08:09:59 -0500

    Html syntax fix and check login form for required

--- src/ui/scripts/pages/index.js
+++ src/ui/scripts/pages/index.js
@@ -4,25 +4,30 @@ export async function indexPage() {
   const auth = new GoTrue();
   const user = auth.currentUser();
 
-  const formTargetEl = document.querySelector('form');
+  const formEl = document.querySelector('form');
   const googleButtonEl = document.querySelector('#googleAuth');
   const errorMessageTargetEl = document.querySelector('#error-message');
   const emailEl = document.querySelector('#email');
   const passwordEl = document.querySelector('#password');
 
   if (user) {
-    window.location = formTargetEl.action;
+    window.location = formEl.action;
     return;
   }
 
-  formTargetEl.addEventListener('submit', (e) => {
+  formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     errorMessageTargetEl.classList.add('d-none');
+
+    if (formEl.checkValidity() === false) {
+      e.stopPropagation();
+      formEl.classList.add('was-validated');
+    }
 
     auth
       .login(emailEl.value, passwordEl.value, true)
       .then(() => {
-        window.location = formTargetEl.action;
+        window.location = formEl.action;
       })
       .catch((err) => {
         errorMessageTargetEl.classList.remove('d-none');
